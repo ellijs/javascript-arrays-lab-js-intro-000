04812abee9a331c47aa6825fa09d640d0ede624a
ji sun lee <ellijs@hotmail.com>
2021-03-23 04:41:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-var kittens = ["Milo", "Otis", "Garfield"] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
-// Add your functions and code here
+
+function destructivelyAppendKitten(name) {
+  kitten.push(name);
+  return kitten;
+}
+
+fnction destructivelyPrependKitten(name) {
+  kitten.unshift(name);
+  return kitten;
+}
+
+function destructivelyRemoveLastKitten() {
+  kitten.pop();
+  return kitten;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kitten.shift();
+  return kitten;
+}
+
+function appendKitten(name) {
+  kitten.concat(name);
+  return kitten;
+}
+
+function prependKitten(name) {
+  kitten.slice(0, 0, name);
+  return kitten;
+}
